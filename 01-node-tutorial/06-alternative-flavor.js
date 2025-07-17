d7689cab09322acc30b2ae113a688fb6d0cb2fd0
Kimhok <ckh.kimhok@gmail.com>
2025-07-17 16:56:43 +0900

    finished lesson one

--- 01-node-tutorial/06-alternative-flavor.js
+++ 01-node-tutorial/06-alternative-flavor.js
@@ -1,6 +1,8 @@
 module.exports.items = ['item1', 'item2']
 const person = {
   name: 'bob',
+  age: 89,
+  job: 'Teacher'
 }
 
 module.exports.singlePerson = person
--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,6 @@
-console.log('Welcome to Node Tutorial')
+const lo = require('lodash');
+
+const items = [1,[2,[3,[4,[5]]]]]
+const newItems = lo.flattenDeep(items);
+
+console.log(newItems);
